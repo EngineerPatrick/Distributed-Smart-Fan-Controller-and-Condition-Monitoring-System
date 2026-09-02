1a04504b86c77cbaa02dfd958415f9af19be6386
Patrick Ferretti <patrickferretti@tuta.io>
2026-09-02 17:17:07 +0200

    Character framebuffer abstraction module improvement
    Change the display abstraction to the character framebuffer abstraction
    
    Reduce complexity and increased safety of initialization logic
    
    Enforce Zephyr's CBF API own architecture by treating the CBF as a single object for all monochrome displays
    
    Add storage for Zephyr's CFB API error codes
    
    Remove alias information to decouple the character framebuffer abstraction from a future display abstraction
    
    Improve logic for class methods

--- src/display_test.cpp
+++ src/display_test.cpp
@@ -1,44 +1,52 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/device.h>
-#include "display.hpp"
+#include "char_framebuff.hpp"
+
+#define DISPLAY0_ALIAS readings_display
 
 static const struct gpio_dt_spec error_led = GPIO_DT_SPEC_GET(DT_ALIAS(red_led), gpios);
 
 int main(void) {
-	display_errors error_code = DISPLAY_ERR_OK;
+	DisplayErrors error_code = DisplayErrors::Ok;
 
 	gpio_pin_configure_dt(&error_led, GPIO_OUTPUT_ACTIVE);
 
-	static monochrome_display_controller readings_display_controller{DEVICE_DT_GET(DT_ALIAS(readings_display)), "readings_display"};
+	static MonochromeDisplay readings_display{DEVICE_DT_GET(DT_ALIAS(DISPLAY0_ALIAS))};
 
-	error_code = readings_display_controller.display.screen_clear();
+	error_code = readings_display.screen_clear();
 
-	if (error_code != DISPLAY_ERR_OK) {
+	if (error_code != DisplayErrors::Ok) {
 		gpio_pin_toggle_dt(&error_led);
 
 		while (1) {}
 	}
 
-	error_code = readings_display_controller.display.string_print("UPCOMING", 0, 2);
+	error_code = readings_display.string_load("UPCOMING", 0, 2);
 
-	if (error_code != DISPLAY_ERR_OK) {
+	if (error_code != DisplayErrors::Ok) {
 		gpio_pin_toggle_dt(&error_led);
 
 		while (1) {}
 	}
 
-	error_code = readings_display_controller.display.string_print("PROJECT!", 1, 2);
+	error_code = readings_display.string_load("PROJECT!", 1, 2);
 
-	if (error_code != DISPLAY_ERR_OK) {
+	if (error_code != DisplayErrors::Ok) {
 		gpio_pin_toggle_dt(&error_led);
 
 		while (1) {}
 	}
 
-	error_code = readings_display_controller.display.string_print("STAY TUNED:)", 3, 0);
+	error_code = readings_display.string_load("STAY TUNED:)", 3, 0);
 
-	if (error_code != DISPLAY_ERR_OK) {
+	if (error_code != DisplayErrors::Ok) {
+		gpio_pin_toggle_dt(&error_led);
+	}
+
+	error_code = readings_display.screen_print();
+
+	if (error_code != DisplayErrors::Ok) {
 		gpio_pin_toggle_dt(&error_led);
 	}
 
