a52f76fdef7e43008528b81ced406131d2b277a5
Patrick Ferretti <patrickferretti@tuta.io>
2026-09-06 17:17:15 +0200

    Character framebuffer abstraction module improvement
    Add system state flags and erorrs to separate healthy checks from last reported error
    
    Improve methods' logic
    
    Improve input logic
    
    Improve initialization logic

--- src/character_framebuffer.cpp
+++ src/character_framebuffer.cpp
@@ -5,52 +5,57 @@
 #include <zephyr/device.h>
 #include <zephyr/display/cfb.h>
 
-z_cbf::CharacterFramebuffer::TargetDisplay z_cbf::CharacterFramebuffer::init_operations(const struct device* const monochrome_display_device_ptr) {
+character_framebuffer::CharacterFramebuffer::TargetDisplay character_framebuffer::CharacterFramebuffer::init_operations(const struct device* const monochrome_display_device_ptr) {
 
 	if (!device_is_ready(monochrome_display_device_ptr)) {
-		this->error = {z_cbf::ErrorCode::DeviceUnready, 0};
+		this->error = {character_framebuffer::ErrorCode::DeviceUnready, 0, 0, 0};
 		return {};
 	}
 
 	this->error.return_value = cfb_framebuffer_init(monochrome_display_device_ptr);
 
 	if (this->error.return_value != 0) {
-		this->error.code = z_cbf::ErrorCode::CfbInit;
+		this->error.code = character_framebuffer::ErrorCode::CfbInit;
+		this->error.last_loaded_row_idx = 0;
+		this->error.last_loaded_column_idx = 0;
 		return {};
 	}
 
-	this->system.cfb_init_flag = 1;
+	this->system.cfb_init = true;
 	TargetDisplay display{{monochrome_display_device_ptr},
 	{static_cast<size_t>(cfb_get_display_parameter(monochrome_display_device_ptr, CFB_DISPLAY_WIDTH))},
 	{static_cast<size_t>(cfb_get_display_parameter(monochrome_display_device_ptr, CFB_DISPLAY_HEIGHT))}};
 
 	if (!display.width_px || !display.height_px) {
-		this->error = {z_cbf::ErrorCode::DisplayResolution, 0};
-		return {};
+		this->error = {character_framebuffer::ErrorCode::DisplayResolution, 0, 0, 0};
+		return display;
 	}
 
-	this->system.cfb_ready_flag = 1;
-	this->error = {z_cbf::ErrorCode::Ok, 0};
+	this->system.cfb_ready = true;
+	this->error = {character_framebuffer::ErrorCode::Ok, 0, 0, 0};
 	return display;
 
 }
 
-z_cbf::ErrorCode z_cbf::CharacterFramebuffer::font_set(uint8_t font_idx) {
+character_framebuffer::ErrorCode character_framebuffer::CharacterFramebuffer::font_set(uint8_t font_idx) {
 
-	if (!this->system.cfb_ready_flag) {
+	if (!this->system.cfb_ready) {
+		this->error = {character_framebuffer::ErrorCode::CfbUnready, 0, 0, 0};
 		return this->error.code;
 	}
 
 	if (font_idx >= cfb_get_numof_fonts(this->display.device_ptr)) {
-		this->error = {z_cbf::ErrorCode::ParamFontIndex, 0};
+		this->error = {character_framebuffer::ErrorCode::ParamFontIndex, 0, 0, 0};
 		return this->error.code;
 	}
 
-	this->system.font_ready_flag = 0;
+	this->system.font_ready = false;
 	this->error.return_value = cfb_framebuffer_set_font(this->display.device_ptr, font_idx);
 
 	if (this->error.return_value != 0) {
-		this->error.code = z_cbf::ErrorCode::CfbFontSet;
+		this->error.code = character_framebuffer::ErrorCode::CfbFontSet;
+		this->error.last_loaded_row_idx = 0;
+		this->error.last_loaded_column_idx = 0;
 		return this->error.code;
 	}
 
@@ -58,116 +63,136 @@ z_cbf::ErrorCode z_cbf::CharacterFramebuffer::font_set(uint8_t font_idx) {
 	this->error.return_value = cfb_get_font_size(this->display.device_ptr, this->font.idx, &(this->font.width_px), &(this->font.height_px));
 
 	if (this->error.return_value != 0) {
-		this->error.code = z_cbf::ErrorCode::CfbFontSizeGet;
+		this->error.code = character_framebuffer::ErrorCode::CfbFontSizeGet;
+		this->error.last_loaded_row_idx = 0;
+		this->error.last_loaded_column_idx = 0;
+		return this->error.code;
+	}
+
+	if (!this->font.width_px ||!this->font.height_px) {
+		this->error.code = character_framebuffer::ErrorCode::CfbFontSizeGet;
+		this->error.last_loaded_row_idx = 0;
+		this->error.last_loaded_column_idx = 0;
 		return this->error.code;
 	}
 
 	if (this->display.width_px < this->font.width_px || this->display.height_px < this->font.height_px) {
-		this->error = {z_cbf::ErrorCode::DisplayResolution, 0};
+		this->error = {character_framebuffer::ErrorCode::DisplayResolution, 0, 0, 0};
 		return this->error.code;
 	}
 
-	this->system.font_ready_flag = 1;
-	this->error = {z_cbf::ErrorCode::Ok, 0};
+	this->system.font_ready = true;
+	this->error = {character_framebuffer::ErrorCode::Ok, 0, 0, 0};
 	return this->error.code;
 }
 
-z_cbf::CharacterFramebuffer::CharacterFramebuffer(const struct device* const monochrome_display_device_ptr) :
+character_framebuffer::CharacterFramebuffer::CharacterFramebuffer(const struct device* const monochrome_display_device_ptr) :
 display{init_operations(monochrome_display_device_ptr)} {
 
-	if (!this->system.cfb_ready_flag) {
+	if (!this->system.cfb_ready) {
 		return;
 	}
 
 	this->error.return_value = cfb_set_kerning(this->display.device_ptr, 0);
 
 	if (this->error.return_value != 0) {
-		this->error.code = z_cbf::ErrorCode::CfbFontKerningSet;
+		this->error.code = character_framebuffer::ErrorCode::CfbFontKerningSet;
+		this->error.last_loaded_row_idx = 0;
+		this->error.last_loaded_column_idx = 0;
 		return;
 	}
 
-	if (this->font_set(0) != z_cbf::ErrorCode::Ok) {
+	if (this->font_set(0) != character_framebuffer::ErrorCode::Ok) {
 		return;
 	}
 
-	this->error = {z_cbf::ErrorCode::Ok, 0};
+	this->error = {character_framebuffer::ErrorCode::Ok, 0, 0, 0};
 }
 
-z_cbf::CharacterFramebuffer::~CharacterFramebuffer() {
+character_framebuffer::CharacterFramebuffer::~CharacterFramebuffer() {
 
-	if (this->system.cfb_init_flag) {
+	if (this->system.cfb_init) {
 		cfb_framebuffer_deinit(this->display.device_ptr);
 	}
 }
 
-z_cbf::ErrorCode z_cbf::CharacterFramebuffer::ram_clear() {
+character_framebuffer::ErrorCode character_framebuffer::CharacterFramebuffer::ram_clear() {
 
-	if (!this->system.cfb_ready_flag) {
+	if (!this->system.cfb_ready) {
+		this->error = {character_framebuffer::ErrorCode::CfbUnready, 0, 0, 0};
 		return this->error.code;
 	}
 
 	this->error.return_value = cfb_framebuffer_clear(this->display.device_ptr, false);
 
 	if (this->error.return_value != 0) {
-		this->error.code = z_cbf::ErrorCode::CfbRamClear;
+		this->error.code = character_framebuffer::ErrorCode::CfbRamClear;
+		this->error.last_loaded_row_idx = 0;
+		this->error.last_loaded_column_idx = 0;
 		return this->error.code;
 	}
 
-	this->error = {z_cbf::ErrorCode::Ok, 0};
+	this->error = {character_framebuffer::ErrorCode::Ok, 0, 0, 0};
 	return this->error.code;
 }
 
-z_cbf::ErrorCode z_cbf::CharacterFramebuffer::string_load(const std::string_view input_string, const size_t row_idx, const size_t column_idx) {
+character_framebuffer::ErrorCode character_framebuffer::CharacterFramebuffer::string_load(std::string_view input_string, const size_t row_idx, const size_t column_idx) {
+	char input_letter[2] = {' ', '\0'};
 
-	if (!this->system.font_ready_flag) {
+	if (!this->system.font_ready) {
+		this->error = {character_framebuffer::ErrorCode::CfbFontUnready, 0, 0, 0};
 		return this->error.code;
 	}
 
 	if (row_idx > (this->display.height_px / this->font.height_px) - 1 || column_idx > (this->display.width_px / this->font.width_px) - 1) {
-		this->error = {z_cbf::ErrorCode::ParamPositionIndex, 0};
+		this->error = {character_framebuffer::ErrorCode::ParamPositionIndex, 0, 0, 0};
 		return this->error.code;
 	}
 
 	if (input_string.size() > (this->display.width_px / this->font.width_px) - column_idx) {
-		this->error = {z_cbf::ErrorCode::ParamStringLength, 0};
+		this->error = {character_framebuffer::ErrorCode::ParamStringLength, 0, 0, 0};
 		return this->error.code;
 	}
 
-	if (input_string.at(input_string.size()) != '\0') {
-		this->error = {z_cbf::ErrorCode::ParamStringNull, 0};
-		return this->error.code;
+	for (size_t i = 0; i < input_string.size(); i++) {
+		input_letter[0] = input_string.at(i);
+
+		this->error.return_value = cfb_draw_text(this->display.device_ptr, input_letter,
+		static_cast<int16_t>((column_idx + i) * this->font.width_px),
+		static_cast<int16_t>(row_idx * this->font.height_px));
+
+		if (this->error.return_value != 0) {
+			this->error.code = character_framebuffer::ErrorCode::CfbStringLoad;
+			this->error.last_loaded_row_idx = row_idx;
+			this->error.last_loaded_column_idx = (column_idx + i);
+			return this->error.code;
+		}
 	}
 
-	this->error.return_value = cfb_draw_text(this->display.device_ptr, input_string.data(),
-	static_cast<int16_t>(column_idx * this->font.width_px),
-	static_cast<int16_t>(row_idx * this->font.height_px));
-
-	if (this->error.return_value != 0) {
-		this->error.code = z_cbf::ErrorCode::CfbStringLoad;
-		return this->error.code;
-	}
-
-	this->error = {z_cbf::ErrorCode::Ok, 0};
+	this->error = {character_framebuffer::ErrorCode::Ok, 0, 0, 0};
 	return this->error.code;
 }
 
-z_cbf::ErrorCode z_cbf::CharacterFramebuffer::ram_write() {
+character_framebuffer::ErrorCode character_framebuffer::CharacterFramebuffer::ram_write() {
 
-	if (!this->system.cfb_ready_flag) {
+	if (!this->system.cfb_ready) {
+		this->error = {character_framebuffer::ErrorCode::CfbUnready, 0, 0, 0};
 		return this->error.code;
 	}
 
 	this->error.return_value = cfb_framebuffer_finalize(this->display.device_ptr);
 
 	if (this->error.return_value != 0) {
-		this->error.code = z_cbf::ErrorCode::CfbRamWrite;
+		this->error.code = character_framebuffer::ErrorCode::CfbRamWrite;
+		this->error.last_loaded_row_idx = 0;
+		this->error.last_loaded_column_idx = 0;
 		return this->error.code;
 	}
 
-	this->error = {z_cbf::ErrorCode::Ok, 0};
+	this->error = {character_framebuffer::ErrorCode::Ok, 0, 0, 0};
 	return this->error.code;
 }
 
-z_cbf::ErrorState z_cbf::CharacterFramebuffer::error_state_get() const {
+character_framebuffer::ErrorState character_framebuffer::CharacterFramebuffer::error_state_get() const {
 		return this->error;
 }
--- src/display_test.cpp
+++ src/display_test.cpp
@@ -6,16 +6,16 @@
 #define DISPLAY0_ALIAS readings_display
 
 int main(void) {
-	z_cbf::ErrorCode error_code = z_cbf::ErrorCode::Ok;
+	character_framebuffer::ErrorCode error_code = character_framebuffer::ErrorCode::Ok;
 	static const struct gpio_dt_spec error_led = GPIO_DT_SPEC_GET(DT_ALIAS(red_led), gpios);
 
 	gpio_pin_configure_dt(&error_led, GPIO_OUTPUT_ACTIVE);
 
-	static z_cbf::CharacterFramebuffer cb0{DEVICE_DT_GET(DT_ALIAS(DISPLAY0_ALIAS))};
+	static character_framebuffer::CharacterFramebuffer cb0{DEVICE_DT_GET(DT_ALIAS(DISPLAY0_ALIAS))};
 
 	error_code = cb0.ram_clear();
 
-	if (error_code != z_cbf::ErrorCode::Ok) {
+	if (error_code != character_framebuffer::ErrorCode::Ok) {
 		gpio_pin_toggle_dt(&error_led);
 
 		while (1) {}
@@ -23,7 +23,7 @@ int main(void) {
 
 	error_code = cb0.string_load("UPCOMING", 0, 2);
 
-	if (error_code != z_cbf::ErrorCode::Ok) {
+	if (error_code != character_framebuffer::ErrorCode::Ok) {
 		gpio_pin_toggle_dt(&error_led);
 
 		while (1) {}
@@ -31,7 +31,7 @@ int main(void) {
 
 	error_code = cb0.string_load("PROJECT!", 1, 2);
 
-	if (error_code != z_cbf::ErrorCode::Ok) {
+	if (error_code != character_framebuffer::ErrorCode::Ok) {
 		gpio_pin_toggle_dt(&error_led);
 
 		while (1) {}
@@ -39,13 +39,13 @@ int main(void) {
 
 	error_code = cb0.string_load("STAY TUNED:)", 3, 0);
 
-	if (error_code != z_cbf::ErrorCode::Ok) {
+	if (error_code != character_framebuffer::ErrorCode::Ok) {
 		gpio_pin_toggle_dt(&error_led);
 	}
 
 	error_code = cb0.ram_write();
 
-	if (error_code != z_cbf::ErrorCode::Ok) {
+	if (error_code != character_framebuffer::ErrorCode::Ok) {
 		gpio_pin_toggle_dt(&error_led);
 
 		while(1) {}
@@ -53,7 +53,7 @@ int main(void) {
 
 	error_code = cb0.string_load("ERROR TRIGGETING", 0, 0);
 
-	if (error_code != z_cbf::ErrorCode::ParamStringLength) {
+	if (error_code != character_framebuffer::ErrorCode::ParamStringLength) {
 		gpio_pin_toggle_dt(&error_led);
 
 		while(1) {}
@@ -61,7 +61,7 @@ int main(void) {
 
 	error_code = cb0.font_set(99);
 
-	if (error_code != z_cbf::ErrorCode::ParamFontIndex) {
+	if (error_code != character_framebuffer::ErrorCode::ParamFontIndex) {
 		gpio_pin_toggle_dt(&error_led);
 
 		while(1) {}
